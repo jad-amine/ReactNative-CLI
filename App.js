bb4d3abb19ea48a993ec936fd13540b6479289d3
jad amine <jade1992amine@gmail.com>
2022-09-29 10:33:12 +0300

    Stack Navigator

--- App.js
+++ App.js
@@ -1,48 +1,8 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React, {useState} from 'react';
-import {Button, StyleSheet, Switch, Text, View} from 'react-native';
+import AppNavContainer from './src/navigations';
 
 const App = () => {
-  const [isEnabled, setIsEnabled] = useState(false);
-
-  const toggleSwitch = () => {
-    console.log('hi');
-    setIsEnabled(!isEnabled);
-  };
-
-  return (
-    <View style={styles.home}>
-      <Text>Hello world!</Text>
-      <Switch
-        trackColor={{false: '#767577', true: '#81b0ff'}}
-        thumbColor={true ? '#f5dd4b' : '#f4f3f4'}
-        ios_backgroundColor="#3e3e3e"
-        onValueChange={toggleSwitch}
-        value={isEnabled}
-      />
-      <Button
-        title="Error"
-        // onPress={() => {
-        //   throw new Error('Empty Response');
-        // }}
-      />
-    </View>
-  );
+  return <AppNavContainer />;
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-  home: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
---
+++ src/navigations/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {Text} from 'react-native';
+
+export default function AppNavContainer() {
+  return (
+    <NavigationContainer>
+      <Text>Hello world!</Text>
+    </NavigationContainer>
+  );
+}
