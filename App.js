42e19be11c34edfd7b45a96484f70511492a9493
jad amine <jade1992amine@gmail.com>
2022-09-29 10:48:14 +0300

    HomeStack Navigator

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import AppNavContainer from './src/navigations';
 
 const App = () => {
---
+++ src/navigations/HomeNavigator.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Text, View} from 'react-native';
+
+const HomeStack = createNativeStackNavigator();
+
+function Contacts() {
+  return (
+    <View>
+      <Text>Hello from Contacts</Text>
+    </View>
+  );
+}
+
+function ContactDetail() {
+  return (
+    <View>
+      <Text>ContactDetail</Text>
+    </View>
+  );
+}
+
+function CreateContact() {
+  return (
+    <View>
+      <Text>CreateContact</Text>
+    </View>
+  );
+}
+
+function Settings() {
+  return (
+    <View>
+      <Text>Settings</Text>
+    </View>
+  );
+}
+
+function HomeNavigator() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Contacts" component={Contacts} />
+      <HomeStack.Screen name="Contact Detail" component={ContactDetail} />
+      <HomeStack.Screen name="Create Contact" component={CreateContact} />
+      <HomeStack.Screen name="Settings" component={Settings} />
+    </HomeStack.Navigator>
+  );
+}
